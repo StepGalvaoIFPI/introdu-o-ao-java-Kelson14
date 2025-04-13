7457c042314a77cac4f2506b6b22957b0952f46f
Kelson14 <kkelsonandre1@gmail.com>
2025-04-13 20:27:11 -0300

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -1,1 +1,18 @@
-//teste
+import java.util.Scanner;
+
+public class Q1{
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite a altura:");
+        int altura = Integer.valueOf(entrada.nextLine());
+
+        System.out.println("Digite a base:");
+        int base = Integer.valueOf(entrada.nextLine());
+
+        double area = (base * altura)/2;
+        System.out.println(area);
+
+        entrada.close();
+    }
+}
